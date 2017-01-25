0b13ded4619f5fe5123b6a565299d66494f046e0
ashes999 <ashes999@yahoo.com>
2017-01-25 00:21:18 -0500

    Ability to create doorways Refactored creation of doors

--- src/entities/door.js
+++ src/entities/door.js
@@ -1,59 +1,65 @@
-
+const DOOR_LENGTH = 64;
+const DOOR_WIDTH = 16;
 
 Crafty.c('Door', {
     init: function() {
-        this.requires("Actor").color('#dddddd').size(DOOR_SIZE, 16);
+        this.requires("Actor").color('#dddddd').size(DOOR_LENGTH, DOOR_WIDTH);
         this.isLocked = false;
     },
     
-    lock: function()
-    {
+    lock: function() {
         this.isLocked = true;
         this.color("#ffbb00");
         return this;
     },
 
     vertical: function() {
-        this.size(16, DOOR_SIZE);
+        this.size(16, DOOR_LENGTH);
         return this;
     },
 })
 
-Crafty.c("WallWithDoor", {
+Crafty.c("WallWithDoorway", {
     create: function(width, height, x, y) {
-        if (typeof(x) == "undefined") 
-        {
+        if (typeof(x) == "undefined") {
             x = 0;
         }
-        if (typeof(y) === "undefined")
-        {
+        if (typeof(y) === "undefined") {
             y = 0;
         }
-        if (width > height)
-        {
-            var wallSize = (width - DOOR_SIZE) / 2;
+        if (width > height) {
+            this.orientation = "horizontal";
+            var wallSize = (width - DOOR_LENGTH) / 2;
             var w1 = Crafty.e("Wall").size(wallSize, height);
-            var door = Crafty.e("Door");
             var w2 = Crafty.e("Wall").size(wallSize, height);
             w1.x = x;
             w1.y = y;
-            door.x = w1.x + w1.width();
-            door.y = w1.y - door.height() / 4;
-            w2.x = door.x + door.width();
+            w2.x = x + width - w2.width();
             w2.y = w1.y;
-        }
-        else
-        {
-            var wallSize = (height - DOOR_SIZE) / 2;
+
+            this.doorwayX = w1.x + w1.width();
+            this.doorwayY = w1.y - (DOOR_WIDTH / 4);
+        } else {
+            this.orientation = "vertical";
+            var wallSize = (height - DOOR_LENGTH) / 2;
             var w1 = Crafty.e("Wall").size(width, wallSize);
-            var door = Crafty.e("Door").vertical();
             var w2 = Crafty.e("Wall").size(width, wallSize);
             w1.x = x;
             w1.y = y;
-            door.x = w1.x - door.width() / 4;
-            door.y = w1.y + w1.height();
             w2.x = w1.x;
-            w2.y = door.y + door.height();
+            w2.y = y + height - w2.height();
+
+            this.doorwayX = w1.x - (DOOR_WIDTH / 4);
+            this.doorwayY = w1.y + w1.height();            
+        }
+        return this;
+    },
+
+    door: function() {
+        var door = Crafty.e("Door").move(this.doorwayX, this.doorwayY);
+        if (this.orientation == "vertical")
+        {
+            door.vertical();
         }
         return door;
     }
--- src/entities/wall.js
+++ src/entities/wall.js
@@ -1,5 +1,3 @@
-const DOOR_SIZE = 64;
-
 Crafty.c('Wall', {
     init: function() {
         this.requires("Actor").color('#443300');
