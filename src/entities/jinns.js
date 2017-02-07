8e1e5b336d7d986060e87d0406712fc003b711e3
ashes999 <ashes999@yahoo.com>
2017-02-07 00:52:21 -0500

    Walker jinn: room to room Stops if doesn't see you Refactor out common jinn-moving code

--- src/entities/jinns.js
+++ src/entities/jinns.js
@@ -1,41 +1,89 @@
 /////////
 // Specific types of jinns
 /////////
-Crafty.c("WandererJinn", {
-    init: function() {
-        var self = this;
-        this.requires("Jinn, ChargePlayerOnSight").size(64, 64).color("#aaffaa");
-        this.pickNewTargetRoom();        
-    },
-
-    pickNewTargetRoom: function() {
-        const BORDER_BUFFER = 16;
-        var targetRoomIndex = randomBetween(0, map.locations.length);
-        targetRoom = map.locations[targetRoomIndex].entity;
-        targetX = randomBetween(targetRoom.x + BORDER_BUFFER, targetRoom.x + targetRoom.width - (2 * BORDER_BUFFER));
-        targetY = randomBetween(targetRoom.y + BORDER_BUFFER, targetRoom.y + targetRoom.height - (2 * BORDER_BUFFER));
-        
-        // move at a constant speed
-        var velocity = config("jinnVelocity");
-        var distanceInPixels = Math.sqrt(Math.pow(targetX - this.x, 2) + Math.pow(targetY - this.y, 2));
-        var travelTimeInSeconds = distanceInPixels / velocity;
-        this.move(targetX, targetY, travelTimeInSeconds);
-
-        // Calling pickNewTargetRoom recursively causes something to keep accumulating; we end up picking
-        // 1, 2, 4, 8, ... exponentially multiple targets at once. I tried everything and couldn't find
-        // any other way around this. 
-        //
-        // Since we know the travel time, just wait that long, plus the delay time, and move again.
-        this.after(travelTimeInSeconds + config("jinnWaitTimeSeconds"), this.pickNewTargetRoom);
-    },
-})
-
 Crafty.c("WalkerJinn", {
     init: function() {
         var self = this;
+        var player = Crafty("Player");
+        this.moving = false;
+
         this.requires("Jinn, ChargePlayerOnSight").size(32, 32).color("#882222");
+
+        this.bind("EnterFrame", function() {
+            // Hunt on sight. Don't hunt when out of sight.
+            var myRoom = map.findRoomWith(this);
+            var playerRoom = map.findRoomWith(player);
+            if (myRoom == playerRoom && !this.huntingPlayer) {
+                this.huntingPlayer = true;
+            } else if (myRoom != playerRoom && this.huntingPlayer) {
+                this.huntingPlayer = false;
+                // Doesn't immediately stop moving. Walker will continue forward
+                // to the room they last saw the player; when they reach, they will
+                // look for a new adjacent room. This is because the tween ends
+                // and calls into moveToAdjacentRoom; we don't need to do anything.
+            }
+            
+            if (this.huntingPlayer) {
+				this.cancelTween("x");
+				this.cancelTween("y");
+				// Calculating distance and moving at an appropriate speed is overrated.
+				// It's expensive, and we're doing this every frame. never mind that.
+				// Instead, just charge toward the player at a relatively fast rate.
+				// We're going to cancel and re-issue this tween every frame.
+				// We don't want to slow down when super close, so if Close Enough, move Super Fast.
+				if (Math.abs(this.x - player.x) + Math.abs(this.y - player.y) <= 150) {
+					this.move(player.x, player.y, config("jinnHuntPounceTweenTime"));
+				} else {
+					this.move(player.x, player.y, config("jinnHuntTweenTime"));
+				}
+			} else {
+                // Move to adjacent room
+                if (this.moving == false) {
+                    this.moveToAdjacentRoom();
+                }
+            }
+        });
+    },
+
+    moveToAdjacentRoom: function() {
+        this.moving = true;
+        var myRoom = map.findRoomWith(this);
+        var directions = myRoom.data.doorDirections + myRoom.data.openDirections; // string, eg. se => south/east
+        var direction = directions[randomBetween(0, directions.length)];
+        var xOffset = 0;
+        var yOffset  = 0;
+
+        if (direction == "e") { 
+            xOffset = 1;
+        } else if (direction == "w") {
+            xOffset = -1;
+        } else if (direction == "n") {
+            yOffset = -1;
+        } else if (direction == "s") {
+            yOffset = 1;
+        }
+
+        var targetRoom = map.getRoomAt(myRoom.data.x + xOffset, myRoom.data.y + yOffset);
+        this.jinnMove(targetRoom, function() {
+            this.moving = false;
+            this.moveToAdjacentRoom();
+        });
     }
 });
+
+Crafty.c("WandererJinn", {
+    init: function() {
+        var self = this;
+        this.requires("Jinn, ChargePlayerOnSight").size(64, 64).color("#aaffaa");        
+        this.pickNewTargetRoom();    
+    },
+
+    pickNewTargetRoom: function() {
+        var targetRoomIndex = randomBetween(0, map.locations.length);
+        targetRoom = map.locations[targetRoomIndex].entity;
+        this.jinnMove(targetRoom, this.pickNewTargetRoom);
+    },
+})
 
 /////////
 // Base class with common behaviour, from which we build specific subclasses
@@ -93,6 +141,24 @@ Crafty.c('Jinn', {
                 Crafty(e).destroy();
             });
         t2.textColor("white");
+    },
+
+    jinnMove: function(targetRoom, onArriveCallback) {
+        const BORDER_BUFFER = 16;        
+        var targetX = randomBetween(targetRoom.x + BORDER_BUFFER, targetRoom.x + targetRoom.width - (2 * BORDER_BUFFER));
+        var targetY = randomBetween(targetRoom.y + BORDER_BUFFER, targetRoom.y + targetRoom.height - (2 * BORDER_BUFFER));
+        // move at a constant speed
+        var velocity = config("jinnVelocity");
+        var distanceInPixels = Math.sqrt(Math.pow(targetX - this.x, 2) + Math.pow(targetY - this.y, 2));
+        var travelTimeInSeconds = distanceInPixels / velocity;
+        this.move(targetX, targetY, travelTimeInSeconds);
+
+        // Calling pickNewTargetRoom recursively causes something to keep accumulating; we end up picking
+        // 1, 2, 4, 8, ... exponentially multiple targets at once. I tried everything and couldn't find
+        // any other way around this. 
+        //
+        // Since we know the travel time, just wait that long, plus the delay time, and move again.
+        this.after(travelTimeInSeconds + config("jinnWaitTimeSeconds"), onArriveCallback);
     }
 });
 
